4189d1d4a96d0f5ee11b98eefda91beba25eddf4
Andre Monteiro <andre.monteiro@proximitybr.com>
2020-01-17 15:09:58 -0300

    website

---
+++ precache-manifest.bb501c4e4fab1a22aa86a6e19dc68145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a35e4da268f10a563f0dce270253f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a53686b1f51c381dd7",
+    "url": "/static/css/main.4c5fa23a.chunk.css"
+  },
+  {
+    "revision": "672a164e1018ea51579f",
+    "url": "/static/js/2.3630e213.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3630e213.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49a53686b1f51c381dd7",
+    "url": "/static/js/main.e8d547aa.chunk.js"
+  },
+  {
+    "revision": "d5afa5a8483928e71a7c",
+    "url": "/static/js/runtime-main.636458a4.js"
+  }
+]);
